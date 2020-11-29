0e222a154650beaf54f0a8315b47fae29a6aa1d8
Thomas <1998thomas@hotmail.be>
2020-11-29 11:24:42 +0100

    Added Waypoint extraction using widht
    On realsense cam

--- src/sim/ros/python3_ros_ws/src/imitation_learning_ros_package/rosnodes/waypoint_extractor.py
+++ src/sim/ros/python3_ros_ws/src/imitation_learning_ros_package/rosnodes/waypoint_extractor.py
@@ -8,6 +8,8 @@ import numpy as np
 from cv_bridge import CvBridge
 import rospy
 from sensor_msgs.msg import *
+import message_filters
+from tf2_msgs.msg import *
 
 sys.path.append('src/sim/ros/test')
 from std_msgs.msg import *
@@ -27,7 +29,6 @@ class WaypointExtractor:
         self.map1, self.map2 = cv2.fisheye.initUndistortRectifyMap(self.K, self.D, np.eye(3), self.K, self.DIM,
                                                                    cv2.CV_16SC2)
         self.counter = 0
-        self.current_image = []
         self.x_cor = 0
         self.y_cor = 0
         self.z_cor = 0
@@ -37,17 +38,18 @@ class WaypointExtractor:
     # Function to extract the cone out of an image. The part of the cone(s) are binary ones, the other parts are 0.
     # inputs: image and color of cone
     # output: binary of cone
-    def get_cone_binary(self, treshold):
-        image_shape = self.current_image.shape
+    def get_cone_binary(self, current_image, treshold):
+        image_shape = current_image.shape
         binary_image = np.zeros((image_shape[0], image_shape[1]))
         for i in range(image_shape[0]):  # Each row
             for k in range(image_shape[1]):  # Each column
-                if self.current_image[i, k] > treshold:
+                if current_image[i, k] > treshold:
                     binary_image[i, k] = 255
         return binary_image
 
     # Extract the 2d location in the image after segmentation.
     # TODO: loops of get_cone_binary and this function can be written in one.
+
     def get_cone_2d_location(self, bin_im):
         cone_found = False
         im_size = bin_im.shape
@@ -63,7 +65,7 @@ class WaypointExtractor:
             for column in range(im_size[1]):
                 if bin_im[row, column] > 0:
                     if current_width_start == -1:
-                        current_width_start = row
+                        current_width_start = column
                         prev_pix = 1
                     elif prev_pix == 1:
                         current_width += 1
@@ -81,25 +83,38 @@ class WaypointExtractor:
         print(max_width_x)
         print(max_width_y)
         print(max_width_row)
-        return [max_width_x, max_width_y, max_width_row]
+        return [max_width_x-424, +max_width_y+400, max_width_row]
 
     def get_cone_3d_location(self, cone_width_px, cone_width_m, conetop_coor, tune_factor):
-        if self.x_cor >ce  -1:
-        # position relative to the camera in meters.
+        if cone_width_px > 0: #only updates when cone detected
+            # position relative to the camera in meters.
             self.z_cor = cone_width_m * tune_factor / cone_width_px
             self.x_cor = conetop_coor[0] * self.z_cor / tune_factor
             self.y_cor = conetop_coor[1] * self.z_cor / tune_factor
 
+
+    def rotate_coordinates(self, coor, x_angle, z_angle):
+        rotation_matrix = np.array([[np.cos(x_angle), -1*np.sin(z_angle), 0],
+                                    [np.cos(x_angle)*np.sin(z_angle), np.cos(x_angle)*np.cos(z_angle), -1*np.sin(x_angle)],
+                                    [np.sin(x_angle)*np.sin(z_angle), np.sin(x_angle)*np.cos(z_angle), np.cos(x_angle)]])
+        rotated_coor = rotation_matrix.dot(coor)
+        #TODO Should have a return instead of assinment
+        self.x_cor = rotated_coor[0]
+        self.y_cor = rotated_coor[1]
+        self.z_cor = rotated_coor[2]
+
     # Extracts the waypoints (3d location) out of the current image.
     def extract_waypoint(self, image):
         cv_im = self.bridge.imgmsg_to_cv2(image, desired_encoding='passthrough')  # Load images to cv
-        self.current_image = cv2.remap(cv_im, self.map1, self.map2, interpolation=cv2.INTER_LINEAR,
+        current_image = cv2.remap(cv_im, self.map1, self.map2, interpolation=cv2.INTER_LINEAR,
                                        borderMode=cv2.BORDER_CONSTANT)  # Remap fisheye to normal picture
         # Cone segmentation
-        bin_im = self.get_cone_binary(treshold=100)
-        self.counter = self.counter + 1  # Counter to save picture under new name
-        print(self.counter)
-        # cv2.imwrite("all_pics/" + str(self.counter) + ".jpg", bin_im)  # Write images to folder
+        bin_im = self.get_cone_binary(current_image, treshold=100)
+
+        #self.counter = self.counter + 1  # Counter to save picture under new name
+        #if self.counter == 1:
+        #   cv2.imwrite("all_pics/" + str(self.counter) + ".jpg", current_image)  # Write images to folder
+
         # Positioning in 2D of cone parts
         loc_2d = self.get_cone_2d_location(bin_im)
         # Get the position and width of the cone
@@ -107,10 +122,21 @@ class WaypointExtractor:
         # Index of middle of max width
         # Remap 2D locations to 3D using width
 
-        self.get_cone_3d_location(max_width, 0.18, [loc_2d[0], loc_2d[1]], 1585)
+        self.get_cone_3d_location(max_width, 0.18, [loc_2d[0], loc_2d[1]], 366)
         # tunefactor calculated by distance[m]*pixels of ob/seize obj[m]
 
+    # Using the rotation angels of the camera to correct for the drone.
+
+    def update_angles(self, angle):
+        #update de angles and translation info
+        #calculations will be done when service is called
+        x = 0
+        #self.rotate_coordinates(coor, x_angle=1, z_angle=1)
+        #TODO update coordinates using these sensor values
+        #print(angle.transforms)
+
     def handle_cor_req(self, req):
+        #should also transform last coordinates
         print("Request received.")
         print([self.x_cor, self.y_cor, self.z_cor])
         return SendRelCorResponse(self.x_cor, self.y_cor, self.z_cor)
@@ -121,7 +147,8 @@ class WaypointExtractor:
         print("Waiting for request")
 
     def image_subscriber(self):
-        rospy.Subscriber("/camera/fisheye1/image_raw", Image, self.extract_waypoint)  # raw image here
+        rospy.Subscriber("/camera/fisheye1/image_raw", Image, self.extract_waypoint)
+        rospy.Subscriber("/tf", TFMessage, self.update_angles)
 
     def run(self):
         self.image_subscriber()
--- src/sim/ros/python3_ros_ws/src/imitation_learning_ros_package/rosnodes/waypointclient.py
+++ src/sim/ros/python3_ros_ws/src/imitation_learning_ros_package/rosnodes/waypointclient.py
@@ -1,31 +1,48 @@
 #!/usr/bin/env python
 import numpy as np
 import rospy
-import time as ti
 from imitation_learning_ros_package.srv import SendRelCor
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 
 class WaypointClient:
     def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.z = 0
-
-    def run_test(self):
+        self.x_vals = []
+        self.y_vals = []
+        self.z_vals = []
+        self.counter = 0
+        self.sleeptime = 1000 #ms to wait
+        self.fig, self.ax = plt.subplots()
+        self.ln, = plt.plot([], [], 'ro')
+        self.ax.set_xlim(-50, 50)
+        self.ax.set_ylim(0, 50)
+        print('Waiting for service')
         rospy.wait_for_service('rel_cor')
-        test_run = 0
-        while test_run < 3:
-            try:
-                get_rel_cor = rospy.ServiceProxy('rel_cor', SendRelCor)
-                resp1 = get_rel_cor()
-                print([resp1.x, resp1.y, resp1.z])
-            except rospy.ServiceException as e:
-                print("Service call failed: %s" % e)
-            ti.sleep(2)
-            test_run += 1
+        print('service found')
+        self.get_rel_cor = rospy.ServiceProxy('rel_cor', SendRelCor)
+
+    #Retrieves coordinates and plots them
+    def animate(self, not_used):
+        try:
+            resp1 = self.get_rel_cor()
+            self.x_vals.append(resp1.x)
+            self.y_vals.append(resp1.y)
+            self.z_vals.append(resp1.z)
+        except rospy.ServiceException as e:
+            print("Service call failed: %s" % e)
+        self.ln.set_data(self.x_vals, self.z_vals)
+        self.counter += 1
+
+    # Run the plotter using the data of the service.
+    def run_test(self):
+
+        ani = FuncAnimation(self.fig, self.animate, interval=self.sleeptime)  # Call service each sec for 3d loc and plot
+        plt.show() #Needed to show plots
+
 
 if __name__ == '__main__':
     new_wp_client = WaypointClient()
-    print("Requesting COOR")
+    print("Start plotting")
     new_wp_client.run_test()
-    print("Test succeeded")
+    print("Done")
