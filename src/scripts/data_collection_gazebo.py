38eef69f2c042f401cc77ab78ee57c0a1ebb04dd
Thomas <1998thomas@hotmail.be>
2021-03-08 10:41:42 +0100

    Data collection for condor

--- src/scripts/data_collection_gazebo.py
+++ src/scripts/data_collection_gazebo.py
@@ -12,7 +12,7 @@ from src.sim.ros.test.common_utils import TopicConfig, TestPublisherSubscriber
 from std_srvs.srv import Empty as Emptyservice, EmptyRequest
 from gazebo_msgs.msg import ModelState, ModelStates
 from gazebo_msgs.srv import SetModelState
-from geometry_msgs.msg import Twist, Pose
+from geometry_msgs.msg import Pose
 from gazebo_msgs.srv import SpawnModel, DeleteModel
 from src.sim.ros.src.utils import quaternion_from_euler_ZYX
 import numpy as np
@@ -27,8 +27,8 @@ from src.core.config_loader import Config, Parser
 class DataCollectionGazebo:
 
     def __init__(self):
-        self.output_dir = f'/media/thomas/Elements/experimental_data/dept_est/{get_filename_without_extension(__file__)}'
-        # self.output_dir = f'{get_data_dir(os.environ["DATADIR"])}/single_location/{get_filename_without_extension(__file__)}'
+        # self.output_dir = f'/media/thomas/Elements/experimental_data/dept_est/{get_filename_without_extension(__file__)}'
+        self.output_dir = f'{get_data_dir(os.environ["DATADIR"])}/cone_data/{get_filename_without_extension(__file__)}'
         print(self.output_dir)
         os.makedirs(self.output_dir, exist_ok=True)
         config = {
@@ -58,8 +58,10 @@ class DataCollectionGazebo:
             'separate_raw_data_runs': True,
             'store_hdf5': True
         }
+        # Create data saver
         config_datasaver = DataSaverConfig().create(config_dict=config_dict)
         self._data_saver = DataSaver(config=config_datasaver)
+        # Create proxy for needed services.
         rospy.wait_for_service('/gazebo/unpause_physics')
         self._unpause_client = rospy.ServiceProxy('/gazebo/unpause_physics', Emptyservice)
         rospy.wait_for_service('/gazebo/pause_physics')
@@ -70,33 +72,18 @@ class DataCollectionGazebo:
         self._spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_sdf_model', SpawnModel)
         rospy.wait_for_service('gazebo/delete_model')
         self._delete_model = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
-    def set_model_state(self, name: str, position):
-        model_state = ModelState()
-        model_state.pose = Pose()
-        model_state.model_name = name
-        model_state.pose.position.x = position[0]
-        model_state.pose.position.y = position[1]
-        model_state.pose.position.z = position[2]
-        roll = position[3]
-        pitch = position[4]
-        yaw = position[5]
-        model_state.pose.orientation.x, model_state.pose.orientation.y, model_state.pose.orientation.z, \
-            model_state.pose.orientation.w = quaternion_from_euler_ZYX((yaw, pitch, roll))  # around x y z axis
-        self._set_model_state(model_state)
 
-        # send command to change drone pos
-        # unpause client + take image
-        # pause client
-
-    def generate_image(self, total_data):
+    # Collection of data in simulation environment gazebo.
+    # Data is saved as experiments and written to the output folder.
+    # Input: amount of images to produce.
+    def generate_image(self, total_data,create_hdf5=False):
         model_name = rospy.get_param('/robot/model_name')
         cone_name = 'Cone'
-        print(model_name)
         self._set_model_state.wait_for_service()
-        prev_seq_nb = -1
         print('waiting for first image')
         image0 = rospy.wait_for_message('/forward/camera/image', Image)  # test image
         prev_seq_nb = image0.header.seq
+        # Main loop to create images.
         for data_collect_amount in range(total_data):
             if np.random.rand() > 0.5:
                 self.delete_model('Boxed_flyzone')
@@ -106,8 +93,7 @@ class DataCollectionGazebo:
             self._unpause_client(EmptyRequest())
             self.set_model_state(model_name, position)
             time.sleep(0.5)
-            # check location: already adjusted?
-            # unpause and record
+            # Collection of annotated data.
             for sensor in ['camera']:  # collision < wrench, only published when turned upside down
                 image = rospy.wait_for_message('/forward/camera/image', Image)
                 position_gazebo = rospy.wait_for_message('/gazebo/model_states', ModelStates)
@@ -142,8 +128,9 @@ class DataCollectionGazebo:
                 image = None
 
                 self._pause_client(EmptyRequest())
-
-        self._data_saver.create_train_validation_hdf5_files()
+        # At the end, create hdf5_files
+        if create_hdf5:
+            self._data_saver.create_train_validation_hdf5_files()
 
     # Transforms the coordinates using the quaternions that describe the position of the drone
     def generate_annotation_cone_from_quat(self, position_drone, position_cone, quat):
@@ -192,8 +179,8 @@ class DataCollectionGazebo:
         postition.position.y = location[1]
         postition.position.z = location[2]
         # read sdf file
-        path_model_drone_zone = f'/home/thomas/code/imitation-learning-codebase/src/sim/ros/gazebo/models/flyzone_wall/flyzone_wall.sdf'
-        path_model_street = f'/home/thomas/code/imitation-learning-codebase/src/sim/ros/gazebo/models/GE_flyzone/flyzone_GE.sdf'
+        path_model_drone_zone = f'src/sim/ros/gazebo/models/flyzone_wall/flyzone_wall.sdf'
+        path_model_street = f'src/sim/ros/gazebo/models/GE_flyzone/flyzone_GE.sdf'
         if not self.prev_zone:
             path_model = path_model_drone_zone
             self.prev_zone = 1
@@ -204,8 +191,23 @@ class DataCollectionGazebo:
         sdff = file_open.read()
         self._spawn_model_prox("Boxed_flyzone", sdff, "flyzone_GE", postition, "world") #TODO make dynamic naming, hold in dict?
 
+    def set_model_state(self, name: str, position):
+        model_state = ModelState()
+        model_state.pose = Pose()
+        model_state.model_name = name
+        model_state.pose.position.x = position[0]
+        model_state.pose.position.y = position[1]
+        model_state.pose.position.z = position[2]
+        roll = position[3]
+        pitch = position[4]
+        yaw = position[5]
+        model_state.pose.orientation.x, model_state.pose.orientation.y, model_state.pose.orientation.z, \
+            model_state.pose.orientation.w = quaternion_from_euler_ZYX((yaw, pitch, roll))  # around x y z axis
+        self._set_model_state(model_state)
+
     def delete_model(self, model_name):
         self._delete_model(model_name)
+
 
 if __name__ == "__main__":
     arguments = Parser().parse_args()
@@ -219,5 +221,5 @@ if __name__ == "__main__":
 
     data_col = DataCollectionGazebo()
     amount_of_images = 20
-    data_col.generate_image(amount_of_images)
+    data_col.generate_image(amount_of_images,False)
     data_col.finish_collection()
